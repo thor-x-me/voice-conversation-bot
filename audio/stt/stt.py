bf662065416a8c25b2e5105c929cc892e4abdaee
thor-x-me <abhishekverm2018@gmail.com>
2024-10-29 11:37:53 +0530

    groq llm for chat

--- audio/stt/stt.py
+++ audio/stt/stt.py
@@ -6,3 +6,5 @@ def speech_to_text(audio):
     model = whisper.load_model("small")
     result = model.transcribe(audio=audio)
     return result["text"]
+
+# print(speech_to_text(r"C:\Users\thorxme\PycharmProjects\voice-conversation-bot\audio.mp3"))
