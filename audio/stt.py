a088e8751247f3b2ac1cb499b008d04162f6c8b0
thor-x-me <abhishekverm2018@gmail.com>
2024-10-29 11:40:21 +0530

    Revert "groq llm for chat"
    This reverts commit bf662065416a8c25b2e5105c929cc892e4abdaee.

--- audio/stt.py
+++ audio/stt.py
@@ -6,5 +6,3 @@ def speech_to_text(audio):
     model = whisper.load_model("small")
     result = model.transcribe(audio=audio)
     return result["text"]
-
-# print(speech_to_text(r"C:\Users\thorxme\PycharmProjects\voice-conversation-bot\audio.mp3"))
